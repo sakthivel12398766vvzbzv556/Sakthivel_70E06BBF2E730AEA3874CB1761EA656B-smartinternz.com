53b4b538dfa99d948745bb41edeb75900ca26651
sakthivel12398766vvzbzv556 <144776988+sakthivel12398766vvzbzv556@users.noreply.github.com>
2023-09-14 11:49:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+def isleapyear(year):
+  if(year % 4==0 and year % 100 !=0):
+    return True
+  else:
+    return False
+   
+year=2012
+
+if isleapyear(year):
+  print('{} is a leapyear.'. format(year))
+else:
+  print('{} is not a leapyear.'. format(year))
